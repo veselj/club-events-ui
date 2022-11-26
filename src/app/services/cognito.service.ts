9e37ec7f644e03b8959d122ab266c2179f259bee
Jan Vesely <jan.vesely@n-able.com>
2022-11-26 16:59:22 +0000

    Upgrade to angular 15

--- src/app/services/cognito.service.ts
+++ src/app/services/cognito.service.ts
@@ -1,6 +1,6 @@
 import {Injectable, OnInit} from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import Amplify, { Auth } from 'aws-amplify';
+import {Amplify, Auth } from 'aws-amplify';
 
 import { environment } from '../../environments/environment';
 
